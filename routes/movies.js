1adacf53750aa2a9719baa97661de082b85eb9fb
thisisbrianhan <bthan@us.ibm.com>
2015-03-17 19:07:15 -0500

    GET route for /movies, responds with JSON

--- routes/movies.js
+++ routes/movies.js
@@ -1,3 +1,14 @@
 var Movie = require('../models/movie');
 var express = require('express');
 var router = express.Router();
+
+router.route('/movies')
+  .get(function(req, res) {
+    Movie.find(function(err, movies) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.json(movies);
+    });
+  }); 
