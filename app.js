e4f490bb1603086b2b11e098f330af8eccac2438
thisisbrianhan <bthan@us.ibm.com>
2015-03-17 19:30:27 -0500

    Connect MongoDB via Mongoose

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var movies = require('./routes/movies');
 var app = express();
+
+// connect to our database
+// Ideally, you will obtain DB details from a config file
+var dbName = 'movieDB';
+var connectString = 'mongodb://localhost:27107' + dbName;
+
+mongoose.connect(connectionString);
